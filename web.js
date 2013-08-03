d1f6b0f540eda7f7dd33d6de9bf5e306c7b45eee
Barak <barak.segal.x@gmail.com>
2013-08-04 00:35:26 +0300

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+var inFile = "index.html";
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var msg = fs.readFileSync(inFile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
